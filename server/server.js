2913cc0ce48fff85ae9c1223c94c85a3371a309f
dio-min <villaloboskristinesheilam@gmail.com>
2026-05-12 22:03:14 +0800

    .test

--- server/server.js
+++ server/server.js
@@ -18,8 +18,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    process.env.FRONTEND_URL,
-    "http://localhost:5173"
+    process.env.FRONTEND_URL
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
